f498cba9bdb89909f8d2a7e88b618c0afc93efe1
lschweiger <lschweiger939@gmail.com>
2021-03-02 18:17:13 -0500

    Switched reading to pickle instead of bz2

---
+++ python3_code/problem_space_server.py
@@ -0,0 +1,37 @@
+# create_problem_space.py
+import os
+import bz2
+import pickle
+from numpy import loadtxt
+
+
+class create_problem_space(object):
+    
+    def __init__(self,problem_space,simulation_number):
+        self.problem_space = problem_space
+        self.simulation_number = simulation_number
+       
+    def return_NK_problem_space(self):
+            cwd = os.getcwd()
+            os.chdir('/mnt/fe296d38-f1f4-4735-8745-7bd587944d6d/Tragedy_of_the_Network_New/NK_Spaces')
+            ## os.chdir('/afs/ir.stanford.edu/users/c/j/cjgomez/Lazer_Tragedy_of_the_Networks/NK_Spaces')
+            #filename = "NK_space_scores_" + str(self.simulation_number) + ".bz2"
+            filename = "NK_space_scores_" + str(self.simulation_number) + ".pkl"
+            #with bz2.open(cwd+"/NK_Spaces/"+filename, "rt",newline='\n') as bz_file:
+            NK_score=pickle.load(open(filename,'rb'))
+            os.chdir(cwd)
+            ## os.chdir('/afs/ir.stanford.edu/users/c/j/cjgomez/Lazer_Tragedy_of_the_Networks/')
+            return NK_score
+    def return_TSP_problem_space(self):
+            cwd = os.getcwd()    
+            
+            os.chdir(cwd+'/TSP_Spaces')
+            #os.chdir('/afs/ir.stanford.edu/users/c/j/cjgomez/Lazer_Tragedy_of_the_Networks/TSP_Spaces')
+            filename = "TSP_Space_"+str(self.simulation_number)+".txt"
+            TSP_space = loadtxt(filename,delimiter=" ",unpack=False)
+            os.chdir(cwd)
+            #os.chdir('/afs/ir.stanford.edu/users/c/j/cjgomez/Lazer_Tragedy_of_the_Networks/')
+            
+            TSP_space = TSP_space.tolist()
+            return TSP_space
+    

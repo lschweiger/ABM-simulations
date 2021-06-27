7ee02403cac00b519d231ac1c8646360cfff8827
lschweiger <lschweiger939@gmail.com>
2021-06-27 19:49:27 -0400

    added evolving network that excludes the current selected agent

--- python3_code/agent_network_server.py
+++ python3_code/agent_network_server.py
@@ -1,5 +1,5 @@
 # network.py
-from random import randint
+import random
 
 
 class create_network(object):
@@ -26,24 +26,30 @@ class create_network(object):
         if self.type_of_network == "single fast communicator agent on torus":
             return [[(i-2)%self.population,(i-1)%self.population,(i+1)%self.population,(i+2)%self.population] for i in range(self.population)]
         if self.type_of_network == "evolving network":
-            evolving_network= [[randint(0,99) for j in range(i+1)] for i in range(self.population)] # redo to skip for loop, must be unique and no self tie
-            for agent_i in range(self.population):
-                for link in range(len(evolving_network[agent_i])):
-                    # Check to see if there are any duplicate links
-                    if len(evolving_network[agent_i])!=len(set(evolving_network[agent_i])):
-                        length = len(evolving_network[agent_i])
-                        evolving_network[agent_i] = list(set(evolving_network[agent_i]))
-                        while len(evolving_network[agent_i]) < length:
-                            evolving_network[agent_i].append(randint(0,99))
-                    #Remove any self links                            
-                    if evolving_network[agent_i][link] == agent_i:
-                        random_link = randint(0,99)
-                        while random_link == agent_i:
-                            random_link = randint(0,99)
-                        evolving_network[agent_i][link] = random_link
+            evolving_network= [random.sample(list(range(0,i))+list(range(i+1,self.population)),i+1) for i in range(0,self.population-1)]
+            evolving_network[0]=[int(round(random.random()*(self.population-1)))+1]
+            while evolving_network[0]==[100]:
+                evolving_network[0]=[int(round(random.random()*(self.population-1)))+1]
+            temp=list(range(self.population-1))
+            random.shuffle(temp)
+            evolving_network.append(temp)
+            # for agent_i in range(self.population):
+            #     for link in range(len(evolving_network[agent_i])):
+            #         # Check to see if there are any duplicate links
+            #         if len(evolving_network[agent_i])!=len(set(evolving_network[agent_i])):
+            #             length = len(evolving_network[agent_i])
+            #             evolving_network[agent_i] = list(set(evolving_network[agent_i]))
+            #             while len(evolving_network[agent_i]) < length:
+            #                 evolving_network[agent_i].append(randint(0,99))
+            #         #Remove any self links                            
+            #         if evolving_network[agent_i][link] == agent_i:
+            #             random_link = randint(0,99)
+            #             while random_link == agent_i:
+            #                 random_link = randint(0,99)
+            #             evolving_network[agent_i][link] = random_link
             return evolving_network
         else:
-            print("error")   
+            print("error") 
  
         
         
